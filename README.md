323ec88f99e00c694e3b127d7e749b34781af9e0
gsilverio <guilhermegsilverio@gmail.com>
2024-04-19 00:54:24 -0300

    Lista 01

---
+++ README.md
@@ -0,0 +1,12 @@
+# Atividades +praTI 📚
+
+Neste repositorio você vai encontrar o codigo fonte de todas as atividades realizadas no decorrer do curso de FullStack ofertada pela +PraTi juntamente com a Codifica, além das
+das informações de como executa-los em sua máquina localmente.
+
+## Lista 01
+>[!TIP]
+>Instruções de como configurar o ambiente e executar os exercícios:
+> - Executar o comando _npm install_ na pasta raiz da atividade para fazer o download das dependencias utilizadas na implementação dos exercícios, tais como o prompt-sync.
+> - Escolher o exercício que queria executar e deletar apenas os Multi-line Comments **[/* */](https://www.w3schools.com/js/js_comments.asp)** dele.
+> - Abrir o terminal e então executar o comando _node lista01.js_.
+> - Após a execução, comentar novamente o exercício e passar para o próximo.
---
+++ lista_01/lista01.js
@@ -0,0 +1,194 @@
+const prompt = require("prompt-sync")();
+
+/*let intOne = parseInt(prompt("Digite o primeiro inteiro: "));
+let intTwo = parseInt(prompt("Digite o segundo inteiro: "));
+let intThree = parseInt(prompt("Digite o terceiro inteiro: "));
+let intFour = parseInt(prompt("Digite o quarto inteiro: "));
+
+intFour = intOne+intTwo+intThree;
+intOne += 25;
+intTwo *= 3;
+intThree *= 1.12;
+
+console.log(intOne, intTwo, intThree.toFixed(2), intFour);*/
+
+
+/*EX 4 e EX 5
+
+let gradeOne = parseFloat(prompt("Digite a primeira nota: "));
+let gradeTwo = parseFloat(prompt("Digite a segunda nota: "));
+
+gradeOne = (gradeOne + gradeTwo)/2;
+
+if(gradeOne>=6.0){
+    console.log("PARABENS!Você foi aprovado.");
+}else {
+    console.log("Você foi REPROVADO! Estude mais.");
+}
+*/
+
+/*EX 06
+
+let sizeA = parseFloat(prompt("Digite o primeiro lado do triangulo: "));
+let sizeB = parseFloat(prompt("Digite o segundo lado do triangulo: "));
+let sizeC = parseFloat(prompt("Digite o terceiro lado do triangulo: "));
+
+if(sizeA < sizeB + sizeC && sizeB < sizeA+sizeC && sizeC<sizeA+sizeB){
+    if(sizeA===sizeB || sizeA===sizeC || sizeB===sizeC ){
+        console.log("Triangulo Isosceles");
+    }else if(sizeA<sizeB && sizeB<sizeC){
+        console.log("Triangulo escaleno");
+    }else if(sizeA===sizeB && sizeA===sizeC){
+        console.log("Triangulo equilatero");
+    }
+} else {
+    console.log("Nao forma triangulo");
+}
+*/
+
+/*EX 7
+
+const MENOR_DUZIA = 0.30;
+const MAIOR_DUZIA = 0.25;
+
+let qtdMaca = parseInt(prompt("Digite a quantidade de maça a ser comprada: "));
+if(qtdMaca>=12){
+    let total = qtdMaca*MAIOR_DUZIA;
+    console.log(typeof(total));
+    console.log("Total: R$ " + total.toFixed(2));
+}else{
+    let total = qtdMaca*MENOR_DUZIA;
+    console.log("Total: R$ "+total.toFixed(2));
+}*/
+
+/*EX 8
+let valOne = parseFloat(prompt("Digite o primeiro valor: "));
+let valTwo = parseFloat(prompt("Digite o segundo valor: "));
+
+if(valOne>valTwo){
+    console.log(`${valTwo}, ${valOne}`);
+} else if(valTwo>valOne){
+    console.log(`${valOne}, ${valTwo}`);
+} else{
+    console.log("Valores são iguais.");
+}*/
+
+/*
+//EX 9
+
+let codigo = parseInt(prompt("Digite o codigo de origem do produto: "));
+if(codigo===5 || codigo ===6){
+    console.log("Nordeste.");
+}else if(codigo>=7 && codigo<=9){
+    console.log("Sudeste.");
+}else if(codigo>=10 && codigo<=20){
+    console.log("Centro-Oeste");
+}else if(codigo>=25 && codigo<=50){
+    console.log("Nordeste.");
+}else{
+    console.log("Produto Importado.");
+}*/
+
+/*
+//EX 10
+
+let num = parseInt(prompt("Digite um numero: "));
+for(let i=0; i<10; i++)
+    console.log(num);
+
+*/
+
+/*
+//EX 11
+
+while(true){
+    let num = parseInt(prompt("Digite um valor: "));
+    if(num<=0)
+        break;
+    else{
+        if(num%2==0)
+            console.log("PAR.");
+        else
+            console.log("IMPAR.");
+    }
+}*/
+
+
+/*
+//EX 12
+
+for(let i=1000; i<=1999; i++){
+    if(i%11===5)
+        console.log(i);
+}
+*/
+
+
+/*
+//EXX 13
+for(let i=0; i<5; i++){
+    let num = parseInt(prompt("Digite um valor: "));
+    for(let x = 1; x<=num; x++){
+        console.log(`${x} x ${num} = ${x*num}`);
+    }
+}
+*/
+
+
+/*
+//EX 14
+let count = 0;
+let total = 0;
+while(true){
+    let num = parseFloat(prompt("Digite um numero: "));
+    if(num===0)
+        break;
+    total+=num;
+    count++;
+}
+console.log(`Media: ${total/count}`)
+*/
+
+/*
+//EX 15
+let total = 0;
+let totalPesos = 0;
+while(true){
+    let num = parseFloat(prompt("Digite um numero: "));
+    if(num===0)
+    break;
+    let peso = parseFloat(prompt("Digite o peso do numero: "));
+
+    num = num*peso;
+    total+=num;
+    totalPesos+=peso;
+}
+if(totalPesos>0)
+    console.log(`Media ponderada: ${(total/totalPesos).toFixed(2)}`);
+else
+    console.log("Media ponderada: " + total.toFixed(2));
+
+*/
+
+//EX 16
+let num = 100;
+let count = 0;
+let countPrimo = 0;
+let i=1;
+while(count<50){
+    while(i<=num){
+        if(num % i === 0)
+        {
+            countPrimo++;
+        }
+        i++;
+    }
+    if(countPrimo==2){
+        console.log("PRIMO: " + num);
+        count++;
+    }
+    i=0;
+    countPrimo=0;
+    num++;
+
+}
